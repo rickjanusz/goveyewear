3dd413b45d6c17959a51e81c1a7ffea8b87f42b7
Arocep Federal <rick@bumblinbee.com>
2026-03-17 21:16:25 -0500

    Update mega menu breadcrumb to brand frames

--- assets/featured-collection-mega-menu.js
+++ assets/featured-collection-mega-menu.js
@@ -305,7 +305,7 @@ class FeaturedCollectionMegaMenu {
     }
     if (this.breadcrumb.brandLabel) {
       if (showFrameCrumb) {
-        this.breadcrumb.brandLabel.textContent = this.activeBrandTitle ? `${this.activeBrandTitle} - Shop by Frame` : '';
+        this.breadcrumb.brandLabel.textContent = this.activeBrandTitle ? `${this.activeBrandTitle} Frames` : '';
       } else {
         this.breadcrumb.brandLabel.textContent = this.activeBrandTitle || '';
       }
